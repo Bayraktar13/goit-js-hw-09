f88af43118e09517091bcd9a82577822f174e5fc
Bayraktar13 <andriihrytsenko@gmail.com>
2023-07-07 13:33:36 +0200

    ДЗ-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -39,6 +39,46 @@
 
 // secondHomeWork();
 
+// function secondHomeWork() {
+//   const formEl = document.querySelector('.form');
+
+//   const onFormElSubmit = event => {
+//     event.preventDefault();
+
+//     const { delay, step, amount } = event.target.elements;
+
+//     for (let i = 1; i <= Number(amount.value); i++) {
+//       const currentDelay = Number(delay.value) + (i - 1) * Number(step.value);
+
+//       createPromise(i, currentDelay)
+//         .then(({ position, delay }) => {
+//           console.log(`✅ Обещание ${position} выполнено за ${delay}мс`);
+//         })
+//         .catch(({ position, delay }) => {
+//           console.log(`❌ Обещание ${position} отклонено за ${delay}мс`);
+//         });
+//     }
+//   };
+
+//   function createPromise(position, delay) {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (shouldResolve) {
+//           resolve({ position, delay });
+//         } else {
+//           reject({ position, delay });
+//         }
+//       }, delay);
+//     });
+//   }
+
+//   formEl.addEventListener('submit', onFormElSubmit);
+// }
+
+// secondHomeWork();
+
 function secondHomeWork() {
   const formEl = document.querySelector('.form');
 
@@ -51,24 +91,24 @@ function secondHomeWork() {
       const currentDelay = Number(delay.value) + (i - 1) * Number(step.value);
 
       createPromise(i, currentDelay)
-        .then(({ position, delay }) => {
-          console.log(`✅ Обещание ${position} выполнено за ${delay}мс`);
+        .then(({ delay }) => {
+          console.log(`✅ Обещание выполнено за ${delay}мс`);
         })
-        .catch(({ position, delay }) => {
-          console.log(`❌ Обещание ${position} отклонено за ${delay}мс`);
+        .catch(({ delay }) => {
+          console.log(`❌ Обещание отклонено за ${delay}мс`);
         });
     }
   };
 
-  function createPromise(position, delay) {
+  function createPromise(delay) {
     const shouldResolve = Math.random() > 0.3;
 
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (shouldResolve) {
-          resolve({ position, delay });
+          resolve({ delay });
         } else {
-          reject({ position, delay });
+          reject({ delay });
         }
       }, delay);
     });
